c6c1a4691585e644e064216f0880238601f570cf
Edwin HC <edwinrhc@gmail.com>
2024-07-13 10:50:31 -0500

    Añadiendo Cart Component

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -2,17 +2,21 @@ import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../models/product";
 import {CatalogComponent} from "../catalog/catalog.component";
+import {CartComponent} from "../cart/cart.component";
+import {CartItem} from "../../models/cartItem";
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent],
+  imports: [CatalogComponent,CartComponent],
   templateUrl: './cart-app.component.html',
   styleUrl: './cart-app.component.css'
 })
 export class CartAppComponent implements OnInit{
 
   products: Product[]  = []; //Iniciamos en un arreglo vacío
+
+  items: CartItem[] = [];
 
     constructor(private service: ProductService) {
     }
---
+++ src/app/models/cartItem.ts
@@ -0,0 +1,10 @@
+import {Product} from "./product";
+
+
+export class CartItem {
+
+  quantity: number = 0;
+  product!: Product;
+
+
+}
