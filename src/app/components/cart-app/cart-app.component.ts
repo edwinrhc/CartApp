3c4f8714600f883698d2f8482f48d141458c45b9
Edwin HC <edwinrhc@gmail.com>
2024-07-25 10:51:07 -0500

    Actualizar la cantidad del producto

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -8,24 +8,39 @@ import {CartItem} from "../../models/cartItem";
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent,CartComponent],
+  imports: [CatalogComponent, CartComponent],
   templateUrl: './cart-app.component.html',
   styleUrl: './cart-app.component.css'
 })
-export class CartAppComponent implements OnInit{
+export class CartAppComponent implements OnInit {
 
-  products: Product[]  = []; //Iniciamos en un arreglo vacío
+  products: Product[] = []; //Iniciamos en un arreglo vacío
 
   items: CartItem[] = [];
 
-    constructor(private service: ProductService) {
-    }
-
-  ngOnInit(): void {
-      this.products  = this.service.findAll();
+  constructor(private service: ProductService) {
   }
 
-  onAddCart(product: Product){
-  this.items = [... this.items, { product:{...product}, quantity: 1 } ];
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
+
+  onAddCart(product: Product) {
+    const hasItem = this.items.find(item => {
+      return item.product.id === product.id;
+    })
+    if (hasItem) {
+      this.items = this.items.map(item => {
+        if(item.product.id === product.id){
+          return {
+            ... item,
+            quantity: item.quantity + 1
+          }
+        }
+        return item;
+      })
+    } else {
+      this.items = [...this.items, {product: {...product}, quantity: 1}];
+    }
   }
 }
